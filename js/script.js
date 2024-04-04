c90067f85438c46b48b9e1fe70ca2c6119506fcc
Mina <hominaa99@gmail.com>
2024-04-04 11:29:07 +0330

    add Js codes

--- js/script.js
+++ js/script.js
@@ -1,1 +1,22 @@
-﻿
+﻿let contextMenu = document.getElementById('contextMenu')
+document.body.addEventListener("contextmenu", function (event) {
+    if (contextMenu.style.display === 'none') {
+        contextMenu.style.display = 'block';
+        contextMenu.style.top = event.pageY + 'px';
+        contextMenu.style.left = event.pageX + 'px';
+    } else {
+        contextMenu.style.top = event.pageY + 'px';
+        contextMenu.style.left = event.pageX + 'px';
+    }
+
+})
+
+document.body.addEventListener("click", function () {
+    contextMenu.style.display = 'none';
+})
+
+document.body.addEventListener("keydown", function (event) {
+    if (event.keyCode === 27) {
+        contextMenu.style.display = 'none';
+    }
+})
